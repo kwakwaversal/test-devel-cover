f2df7f19b8db7c4f0c6f1dd147c81d123172c0b9
Paul Williams <kwakwaversal@gmail.com>
2016-11-03 09:01:38 +0000

    Adding script to prove with Devel::Cover options.

---
+++ test_coverage.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+rm -rf cover_db && rm cover_db.out
+PERL5OPT=-MDevel::Cover=-coverage,pod-also_private-BUILD,statement,branch,condition,path,subroutine,pod,time prove -vwl t
+cover -silent -summary +ignore_re 'prove$|\.t$' | tee cover_db.out
