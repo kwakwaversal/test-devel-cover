a0084c9fe661d3ae81af4b8ceb6b8b424aa7b5d2
Paul Williams <kwakwaversal@gmail.com>
2016-11-03 11:15:41 +0000

    Updating statement coverage tests.

--- lib/Testing/Statement.pm
+++ lib/Testing/Statement.pm
@@ -3,12 +3,49 @@ package Testing::Statement;
 use Moo;
 use Types::Standard qw/:all/;
 
-has foo => (is => 'ro', => isa => Str);
+has name => (is => 'lazy', => isa => Str);
+
+sub meow {
+  my ($self, $loudly) = @_;
+
+  if ($loudly) {
+    return 'MEOW!';
+  }
+
+  return 'meow';
+}
 
 sub roar {
-  my $self = shift;
+  my ($self) = @_;
 
-  return 'ROAR!';
+  return 'roar';
 }
 
 1;
+
+=head1 NAME
+
+Testing::Statement - Testing statement coverage
+
+=head1 DESCRIPTION
+
+L<Testing::Statement> is used to help understand and test metrics reported
+on by L<Devel::Cover>.
+
+=head1 METHODS
+
+L<Testing::Statemtnt> implements the following methods.
+
+=head2 meow
+
+It's what cats do.
+
+=head2 roar
+
+It's what bigger cats do.
+
+=head1 SEE ALSO
+
+L<Devel::Cover>.
+
+=cut
--- t/statement.t
+++ t/statement.t
@@ -5,6 +5,10 @@ use Test::More;
 use Testing::Statement;
 
 my $t = Testing::Statement->new(foo => 'bar');
-is $t->roar(), 'ROAR!';
+is $t->meow(), 'meow';
+is $t->roar(), 'roar';
+
+my $t2 = Testing::Statement->new(foo => 'bar');
+is $t2->meow(1), 'MEOW!';
 
 done_testing;
